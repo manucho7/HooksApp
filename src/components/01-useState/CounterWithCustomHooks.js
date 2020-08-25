dd23b543b09d79273d3ae07893e522093e29de41
Manuel Blanco <manuel.blanco77@gmail.com>
2020-08-25 11:42:11 -0300

    Subiendo Hook useCounter, terminando counterApp con Hooks

--- src/components/01-useState/CounterWithCustomHooks.js
+++ src/components/01-useState/CounterWithCustomHooks.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import "./counter.css";
+import { useCounter } from '../../hooks/useCounter';
 
 export const CounterWithCustomHooks = () => {
+
+    const { state, increment, decrement, reset } = useCounter(100);
+
     return (
         <>
-            <h1>Counter with Hook: { 0 }</h1>
+            <h1>Counter with Hook: { state } </h1>
             <hr></hr>
 
-            <button className="btn"> + 1 </button>
-            <button className="btn"> - 1 </button>
+            <button onClick={ () => increment(2) } className="btn"> + 1 </button>
+            <button onClick={ reset } className="btn"> Reset </button>
+            <button onClick={ () => decrement(2) } className="btn"> - 1 </button>
         </>
     )
 }
