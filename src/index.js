55e73391ca80e35cad178faef88e210c3925d5b4
Manuel Blanco <manuel.blanco77@gmail.com>
2020-08-26 16:44:05 -0300

    Subiendo custom Hook useFetch e implementandolo en MultipleCustomHooks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+//import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 //import { SimpleForm } from '../src/components/02-useEffect/SimpleForm';
 //import { CounterWithCustomHooks } from './components/01-useState/CounterWithCustomHooks';
 //import { HookApp } from './HookApp';
@@ -8,7 +9,7 @@ import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook
 
 
 ReactDOM.render(
-    <FormWithCustomHook />,
+    <MultipleCustomHooks />,
   document.getElementById('root')
 );
 
