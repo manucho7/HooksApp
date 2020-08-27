19f5db159c46cf1076e46c297be547968590b1db
Manuel Blanco <manuel.blanco77@gmail.com>
2020-08-27 13:45:21 -0300

    useCallback Hook y su componente hijo que recibe funcion

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { MemoHook } from './components/06-memos/MemoHook';
+import { CallbackHook } from './components/06-memos/CallbackHook';
+//import { MemoHook } from './components/06-memos/MemoHook';
 //import { Memorize } from './components/06-memos/Memorize';
 //import { LayoutEffect } from './components/05-useLayoutEffect/LayoutEffect';
 //import { RealRef } from './components/04-useRef/RealRef';
@@ -13,7 +14,7 @@ import { MemoHook } from './components/06-memos/MemoHook';
 //import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 
 ReactDOM.render(
-    <MemoHook />,
+    <CallbackHook />,
   document.getElementById('root')
 );
 
