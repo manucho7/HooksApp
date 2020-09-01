365bbda90453ec897547eb5e849350af3e774b1f
Manuel Blanco <manuel.blanco77@gmail.com>
2020-09-01 10:49:14 -0300

    Subiendo optimizacion de componente para no dejar tan cargado el componente

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,7 +1,9 @@
 import React, { useReducer, useEffect } from 'react'
 import { TodoReducer } from './TodoReducer';
 import { useForm } from '../../hooks/useForm';
+import { TodoList } from './TodoList';
 import './styles.css';
+
 
 
 const init = () => {
@@ -66,30 +68,11 @@ export const TodoApp = () => {
             <div className="row">
 
                 <div className="col-7">
-                    <ul className="list-group list-group-flush">
-                        {
-                            todos.map( (todo, i) => (
-                                <li
-                                    key={ todo.id }
-                                    className="list-group-item"
-                                > 
-                                    <p 
-                                        className={`${ todo.done && 'complete' }`}
-                                        onClick={ () => handleToggle(todo.id) }
-                                    >
-                                        { i + 1 }. {todo.desc }
-                                    </p>
-
-                                    <button
-                                        className="btn btn-danger"
-                                        onClick={ () => handleDelete( todo.id ) }
-                                    >
-                                        Borrar
-                                    </button>
-                                </li>
-                            ))
-                        }
-                    </ul>
+                   <TodoList 
+                        todos={ todos }
+                        handleDelete={ handleDelete }
+                        handleToggle={ handleToggle }
+                   />
                 </div>
 
                 <div className="col-5">
