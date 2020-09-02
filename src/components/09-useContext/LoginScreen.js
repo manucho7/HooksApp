57abc18842ebee86092dc091125778e885a3faed
Manuel Blanco <manuel.blanco77@gmail.com>
2020-09-02 13:19:48 -0300

    usando useContext para pasar data de componente padre a hijos

--- src/components/09-useContext/LoginScreen.js
+++ src/components/09-useContext/LoginScreen.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import { useContext } from 'react'
+import { UserContext } from './UserContext';
 
 export const LoginScreen = () => {
+
+    const { setUser } = useContext( UserContext );
+
     return (
         <div>
             <h1>LoginScreen</h1>
             <hr />
 
-
+            <button 
+                className="btn btn-primary"
+                onClick={ () => setUser({
+                    id: 777,
+                    name: 'Manusi'
+                })}
+            >
+                Login
+            </button>
         </div>
     )
 }
--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,18 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AppRouter } from './AppRouter'
 import { UserContext } from './UserContext'
 
 
 export const MainApp = () => {
 
-    const user = {
-        id: 1234,
-        name: 'Manuel',
-        email: 'manuel@gmail.com'
-    }
+    const [user, setUser] = useState({});
 
     return (
-        <UserContext.Provider value={ user }>
+        <UserContext.Provider value={{
+            user,
+            setUser
+        }}>
             <AppRouter />
         </UserContext.Provider>
     )
